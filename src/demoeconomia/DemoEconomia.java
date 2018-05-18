03b17012c08299d9ce3cf31f540f1696985567c9
Alessio <alessiospina93@hotmail.it>
2018-05-18 22:12:08 +0200

    v0.1

---
+++ src/demoeconomia/DemoEconomia.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoeconomia;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author alessio spina
+ */
+public class DemoEconomia {
+                               // peso,prezzo,prestazioni,estetica,comfort
+   // private double[] settore1 = {0.0562,0.1011,0.5169,0.0337,0.2921};
+  //  private double[] settore2 = {0.1264,0.2644,0.3448,0.0920,0.1724};
+   // private double[] settore3 = {0.28,0.08,0.32,0.18,0.14};
+   // private double[] settore4 = {0.0306,0.1224,0.5204,0.2653,0.0612};
+   // private double[] settore5 = {0.055,0.4587,0.2661,0.0275,0.1927};
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        mainFrame m=new mainFrame();
+        m.setVisible(true);
+    }
+
+    /**
+     * @return the hmap_settori
+     */
+   
+    
+}
