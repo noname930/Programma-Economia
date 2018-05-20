61945578c2b20890a2c2e25387eb064deb626f57
Alessio <alessiospina93@hotmail.it>
2018-05-20 21:47:43 +0200

    v0.2

--- src/demoeconomia/DemoEconomia.java
+++ src/demoeconomia/DemoEconomia.java
@@ -12,12 +12,18 @@ import java.util.HashMap;
  * @author alessio spina
  */
 public class DemoEconomia {
-                               // peso,prezzo,prestazioni,estetica,comfort
-   // private double[] settore1 = {0.0562,0.1011,0.5169,0.0337,0.2921};
-  //  private double[] settore2 = {0.1264,0.2644,0.3448,0.0920,0.1724};
-   // private double[] settore3 = {0.28,0.08,0.32,0.18,0.14};
-   // private double[] settore4 = {0.0306,0.1224,0.5204,0.2653,0.0612};
-   // private double[] settore5 = {0.055,0.4587,0.2661,0.0275,0.1927};
+    
+   protected static String settore1 = "Computer molto prestanti e che abbiano un ottimo comfort";
+   protected static String settore2 = "Computer con prestanti medio alte al prezzo giusto";
+   protected static String settore3 = "Computer con prestazioni medie e che abbiano il giusto peso";
+   protected static String settore4 = "Computer molto prestanti e con una bella estetica";
+   protected static String settore5 = "Computer a basso costo e abbastanza prestanti";
+   
+   protected static String[] pc_settore1 = new String[]{"Dell XPS 13","Lenovo Yoga Book","Acer Spin 5","Asus UX360CA-C4171T","Acer Swift 3"};
+   protected static String[] pc_settore2 = new String[]{"Asus Vivobook X556UR-XO344T","HP 250 G5","Acer Aspire F5-573G-54WE","Asus X756UX-T4188T","Acer Swift 3 SF314-52-570N"};
+   protected static String[] pc_settore3 = new String[]{"MacBook 15","MacBook 13","Asus ZenBook","Asus N552VW-FY136T","Dell XPS 15"};
+   protected static String[] pc_settore4 = new String[]{"Alienware 15","MacBook 13","Asus ZenBook","Asus Rog","Microsoft Surface"};
+   protected static String[] pc_settore5 = new String[]{"HP 255 G5","Asus X540SA","Acer Chromebook CB5-571-C4Y3","Asus X556UA-XO607T","Acer CB3-431-C1AN Chromebook"};
     
    
     public static void main(String[] args) {
